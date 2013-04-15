0ff210e138494a2a0b0805c4900115b46cb66115
Croos Tools Project User <dreyer@molgen.mpg.de>
2013-04-15 15:28:35 +0200

    rename files to be more raspberry pi specific

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -10,7 +10,7 @@ export PREFIX_SYSTEM=${PREFIX}
 export PREFIX_CROSSTOOLS=${PREFIX}/cross-tools
 
 export CLFS_HOST=$(echo ${MACHTYPE} | sed -e 's/-[^-]*/-cross/')
-export CLFS_TARGET="armv6-cross-linux-gnueabi"
+export CLFS_TARGET="armv6-bcm2835-linux-gnueabi"
 export CLFS_ABI="aapcs-linux"
 export CLFS_ARCH="arm"
 export CLFS_ENDIAN="little"
@@ -23,15 +23,15 @@ export PATH=${PREFIX_CROSSTOOLS}/bin:/bin:/usr/bin:/usr/local/bin
 export LC_ALL=POSIX
 
 for p in \
-    cross-arm-linux-headers_raspberrypi-3.6.y-0.bee \
-    cross-arm-gmp-5.1.1-0.bee \
-    cross-arm-mpfr-3.1.2-0.bee \
-    cross-arm-mpc-1.0.1-0.bee \
-    cross-arm-binutils-2.23.2-0.bee \
-    cross-arm-gcc_prereq-1-0.bee \
-    cross-arm-gcc_static-4.8.0-0.bee \
-    arm-glibc-2.17-0.bee \
-    cross-arm-gcc-4.8.0-0.bee
+    cross-pi-linux-headers_raspberrypi-3.6.y-0.bee \
+    cross-pi-gmp-5.1.1-0.bee \
+    cross-pi-mpfr-3.1.2-0.bee \
+    cross-pi-mpc-1.0.1-0.bee \
+    cross-pi-binutils-2.23.2-0.bee \
+    cross-pi-gcc_prereq-1-0.bee \
+    cross-pi-gcc_static-4.8.0-0.bee \
+    cross-pi-glibc-2.17-0.bee \
+    cross-pi-gcc-4.8.0-0.bee
     do
 
     if [ -z "${stop_skipping}" ] && bee list ${p%.bee} >/dev/null ; then
@@ -45,7 +45,7 @@ for p in \
 done
 
 for p in \
-    cross-arm-gcc_static-4.8.0-0.bee
+    cross-pi-gcc_static-4.8.0-0.bee
     do
 
     package=${p%.bee}.$(arch)
